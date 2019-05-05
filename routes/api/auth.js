fbb91ea116553c813f32d758f41a0d8b3f3ffba1
MrTj458 <mrtj458@gmail.com>
2019-05-05 13:56:28 -0600

    User log in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator/check')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
 
@@ -16,5 +20,60 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required')
+      .not()
+      .isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+    try {
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email or password is incorrect' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email or password is incorrect' }] })
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+
+      const token = await jwt.sign(payload, config.get('jwtSecret'), {
+        expiresIn: 3600,
+      })
+
+      return res.json({ token })
+    } catch (err) {
+      console.error(err)
+      return res.status(500).send('Server error')
+    }
+  }
+)
 
 module.exports = router
