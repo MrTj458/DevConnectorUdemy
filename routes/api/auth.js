baf75ca0bf231deaa33cab9eac96d02d2599d7c8
MrTj458 <mrtj458@gmail.com>
2019-05-05 13:44:31 -0600

    Get auth user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
 const router = express.Router()
 
 // @route   GET api/auth
 // @desc    Test Route
 // @access  public
-router.get('/', auth, (req, res) => {
-  res.send('Auth Route')
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server Error')
+  }
 })
 
 module.exports = router
